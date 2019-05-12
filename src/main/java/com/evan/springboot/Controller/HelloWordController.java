666f0dc0aa3471bba703c4a7e4073e0e24c702b3
Evan <530788697@qq.com>
2019-05-12 08:50:45 +0800

    init

---
+++ src/main/java/com/evan/springboot/Controller/HelloWordController.java
@@ -0,0 +1,18 @@
+package com.evan.springboot.Controller;
+
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWordController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        JSONObject
+        return "helloWorld";
+    }
+}
