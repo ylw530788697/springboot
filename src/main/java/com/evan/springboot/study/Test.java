d4d8dd8edc3a9655cbbd3cc9f7b8c8238de661cd
ylw530788697 <530788697@qq.com>
2019-11-22 18:12:04 +0800

    整合mybatis-plus

--- src/main/java/com/evan/springboot/study/Test.java
+++ src/main/java/com/evan/springboot/study/Test.java
@@ -1,8 +1,12 @@
 package com.evan.springboot.study;
 
+import com.alibaba.fastjson.JSON;
+
+import java.util.Stack;
+
 public class Test {
     public static void main(String[] args) {
-        int size=10;
+       /* int size=10;
         double[] mylist=new double[size];
         mylist[0]=5.6;
         mylist[1]=1.2;
@@ -10,6 +14,42 @@ public class Test {
         for (int i = 0; i < mylist.length; i++) {
             total+=mylist[i];
         }
-        System.out.println(total);
+        System.out.println(total);*/
+        int[] nums ={2, 7, 11, 15};
+        int target = 9;
+        int[] ints = twoSum(nums,target);
+        System.out.println(JSON.toJSONString(ints));
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if ((nums[i] + nums[j]) == target) {
+                    result[0] = i;
+                    result[1] = j;
+                }
+            }
+        }
+        return result;
+    }
+
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        char[] chars = s.toCharArray();
+        for (char aChar : chars) {
+            if (stack.size() == 0) {
+                stack.push(aChar);
+            } else if (isSym(stack.peek(), aChar)) {
+                stack.pop();
+            } else {
+                stack.push(aChar);
+            }
+        }
+        return stack.size() == 0;
+    }
+
+    private boolean isSym(char c1, char c2) {
+        return (c1 == '(' && c2 == ')') || (c1 == '[' && c2 == ']') || (c1 == '{' && c2 == '}');
     }
 }
