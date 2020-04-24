136ee603a7036969a80775e0229ccd4373973ad0
ylw530788697 <530788697@qq.com>
2020-04-24 18:02:58 +0800

    练习代码

--- src/main/java/com/evan/springboot/designStudy/factory/BenZ.java
+++ src/main/java/com/evan/springboot/designStudy/factory/BenZ.java
@@ -3,11 +3,11 @@ package com.evan.springboot.designStudy.factory;
 /**
  * @author evanYang
  * @version 1.0
- * @date 2020/4/24 下午 5:23
+ * @date 2020/4/24 下午 5:30
  */
-public class BMWCar implements Car{
+public class BenZ implements Car {
     @Override
     public void run() {
-        System.out.println("BMW 车车跑的快快得");
+        System.out.println("大奔就是快");
     }
 }
