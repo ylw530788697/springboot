04cf6bd5a75f1262af823bd8fb86e140cea509ae
Evan <530788697@qq.com>
2022-04-05 22:15:53 +0800

    rocketmq-all-5.0.0-ALPHA-bin-release.zip

---
+++ src/main/java/com/evan/springboot/study/rocketmq/MyRocketMqProducer.java
@@ -0,0 +1,17 @@
+package com.evan.springboot.study.rocketmq;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+
+/**
+ * @author evanYang
+ * @version 1.0
+ * @date 2022/04/04 22:58
+ */
+public class MyRocketMqProducer {
+    public static void main(String[] args) {
+
+        DefaultMQProducer defaultMQProducer = new DefaultMQProducer();
+
+
+    }
+}
